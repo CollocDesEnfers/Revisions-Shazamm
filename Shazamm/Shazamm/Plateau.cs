5695855a760f6d28ba8946d6df83a3d219ccd39f
Gab55 <gabrielgiroud@gmail.com>
2018-12-17 18:41:08 +0100

    update plateau

--- Shazamm/Shazamm/Plateau.cs
+++ Shazamm/Shazamm/Plateau.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,33 @@ namespace Shazamm
 {
     class Plateau
     {
+        int nbCase;
+        int placeJ1;
+        int placeMur;
+        int placeJ2;
         List<string> plateauCase = new List<string>();
 
         public List<string> PlateauCase { get => plateauCase; set => plateauCase = value; }
+        public int NbCase { get => nbCase; set => nbCase = value; }
+        public int PlaceJ1 { get => placeJ1; set => placeJ1 = value; }
+        public int PlaceMur { get => placeMur; set => placeMur = value; }
+        public int PlaceJ2 { get => placeJ2; set => placeJ2 = value; }
 
         public void creationPlateau()
         {
-            for (int i = 1; i < 18; i++) { 
-                if (i == 6)
+            nbCase = 18;
+            placeMur = nbCase / 2;
+            placeJ1 = placeMur - 2;
+            placeJ2 = placeMur + 2;
+            for (int i = 1; i < NbCase; i++) { 
+                if (i == placeJ1)
                 {
                     PlateauCase.Add("[J1]");
                 }
-                if (i == 9) {
+                if (i == placeMur) {
                     PlateauCase.Add("[M]");
                 }
-                if (i == 12)
+                if (i == placeJ2)
                 {
                     PlateauCase.Add("[J2]");
                 }
